f19da1d185fbb31718b8245ea713a93f51ffbf2d
alexandramangasaryan <69577361+alexandramangasaryan@users.noreply.github.com>
2020-08-18 21:49:52 +0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Hello World!");
+        }
+	static void Function(string[] args)
+	{
+	    Console.WriteLine("Hello everyone)");
+	}
+    }
+}
